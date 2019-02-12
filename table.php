961547ce2ede304ad5558c7f64ca6ceb8cd8cff1
duszmox <38438001+duszmox@users.noreply.github.com>
2019-02-12 20:50:22 +0100

    Remove-Favicon
    I removed the not working Favicon

--- table.php
+++ table.php
@@ -67,15 +67,6 @@ if($_GET['table']){
 <head>
 	<title>Ügyviteli rendszer </title>
 
-<link rel="apple-touch-icon" sizes="144x144" href="pophone/favicon/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="pophone/favicon/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="pophone/favicon/favicon-16x16.png">
-<link rel="manifest" href="pophone/favicon/site.webmanifest">
-<link rel="mask-icon" href="pophone/favicon/safari-pinned-tab.svg" color="#157996">
-<link rel="shortcut icon" href="pophone/favicon/favicon.ico">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-config" content="pophone/favicon/browserconfig.xml">
-<meta name="theme-color" content="#ffffff">
 
 
 			
